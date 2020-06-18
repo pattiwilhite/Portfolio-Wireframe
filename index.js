f3d77faa198461af0785e677ef2dc7e40105f86e
Patti Wilhite <56936033+pattiwilhite@users.noreply.github.com>
2020-06-18 11:07:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+unction myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function scrollWin(x, y) {
+    window.scrollBy(x, y);
+}
